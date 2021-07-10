c9f0bec22950ca41e252945d5791f971ddd62815
Bruno Valle <brunolv6@gmail.com>
2021-07-09 21:45:41 -0300

    setup

---
+++ src/main/java/br/com/zupacademy/bruno/trasancao/TrasancaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.bruno.trasancao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasancaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasancaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/bruno/trasancao/TrasancaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.bruno.trasancao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasancaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
